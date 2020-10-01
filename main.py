4e7ba83efc12ab392f9fc8bc965ea68e6afd42a5
Yash Chaudhary <chaudharyyash088@gmail.com>
2020-10-01 22:50:50 +0530

    commit

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 
+import os 
+
+for i in range(10):
+    d = str(i) + " days ago"
+    with open('bot.txt', 'a') as file:
+        file.write(d+'\n')
+    os.system("git add bot.txt")
+    os.system('git commit --date="'+d+'" -m " test commit"')
+os.system('git push -u origin master')    
